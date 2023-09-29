300f5117671315518a2ab5f70fe20c25c9bd97e3
cheelix <cheelix@gmail.com>
2023-09-28 22:43:23 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm  ">
         
-        <h1 >CPRG 306: Web Development 2 - Assignments</h1>
+        <h1 className="text-2xl font-bold mb-4">CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo/>
         <div><Link href='week2'>week2</Link></div>
         <div><Link href='week3'>week3</Link></div>
